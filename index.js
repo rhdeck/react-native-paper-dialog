a78bdc899a0cf9a7dca34afcf98d7a10365f6325
Ray Deck <ray@raydeck.com>
2019-05-31 07:30:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,129 @@
+import React, {
+  createContext,
+  useEffect,
+  useState,
+  useContext,
+  useCallback,
+  Fragment
+} from "react";
+import { ScrollView } from "react-native";
+import { Portal, Dialog, List, Button } from "react-native-paper";
+import Markdown from "react-native-markdown-renderer";
+import Deferred from "es6-deferred";
+const context = createContext();
+const DProvider = context.Provider;
+const DialogProvider = ({ children }) => {
+  const [isDialog, setIsDialog] = useState(false);
+  const [message, setMessage] = useState();
+  const [title, setTitle] = useState();
+  const [actions, setActions] = useState([]);
+  const [cancelText, setCancelText] = useState("cancel");
+  const [dismissKey, setDismissKey] = useState();
+  const clearDismiss = useCallback(() => setDismissKey(null), []);
+  const dismissDialog = useCallback(key => {
+    setDismissKey(key);
+    setIsDialog(false);
+  }, []);
+  const [value, setValue] = useState({
+    cancelText,
+    dismissKey,
+    clearDismiss,
+    isDialog,
+    setMessage,
+    setTitle,
+    setActions,
+    setCancelText,
+    setIsDialog
+  });
+  useEffect(() => {
+    setValue({
+      cancelText,
+      dismissKey,
+      clearDismiss,
+      isDialog,
+      setMessage,
+      setTitle,
+      setActions,
+      setCancelText,
+      setIsDialog
+    });
+  }, [cancelText, dismissKey, clearDismiss, isDialog]);
+  const ret = [
+    <Portal>
+      <Dialog visible={isDialog} onDismiss={() => setIsDialog(false)}>
+        {title && <Dialog.Title>{title}</Dialog.Title>}
+        <Dialog.Content>
+          <Dialog.ScrollArea>
+            {message && (
+              <ScrollView style={{ maxHeight: 300 }}>
+                <Markdown>{message}</Markdown>
+              </ScrollView>
+            )}
+          </Dialog.ScrollArea>
+          <List.Section>
+            {actions.map(({ icon, title, key, description }) => (
+              <List.Item
+                left={() => icon && <List.Icon icon={icon} />}
+                onPress={() => dismissDialog(key)}
+                title={title}
+                description={description}
+              />
+            ))}
+          </List.Section>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={() => dismissDialog("cancel")}>{cancelText}</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>,
+    <DProvider value={value}>{children}</DProvider>
+  ];
+  return ret;
+};
+const withDialog = C => props => {
+  return (
+    <DialogProvider>
+      <C {...props} />
+    </DialogProvider>
+  );
+};
+const useShowDialog = () => {
+  const {
+    dismissKey,
+    clearDismiss,
+    setMessage,
+    setTitle,
+    setActions,
+    setCancelText,
+    setIsDialog
+  } = useContext(context);
+  const showDialog = useCallback(
+    async (
+      { title, cancelText = "Close", actions = [], message = null },
+      callback = null
+    ) => {
+      setTitle(title);
+      setCancelText(cancelText);
+      setActions(actions);
+      setMessage(message);
+      setIsDialog(true);
+      const promise = new Deferred();
+      setPromise(promise);
+      const outkey = await promise.promise;
+      if (typeof callback === "function") return f(outkey);
+      return outkey;
+    },
+    []
+  );
+  const [promise, setPromise] = useState();
+  useEffect(() => {
+    if (promise && dismissKey) {
+      promise.resolve(dismissKey);
+      clearDismiss();
+      setPromise(null);
+    }
+  }, [dismissKey, promise]);
+  return showDialog;
+};
+export default DialogProvider;
+export { useShowDialog, withDialog };
